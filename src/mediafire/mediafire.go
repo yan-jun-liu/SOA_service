7e3638ce80136936b9baedd1c51a8980fb6ee624
Yanjun Liu <yanjun.liu10@outlook.com>
2020-04-07 13:33:35 +0300

    refactor: code reformat in mediafire.go
    - rearrange function sequence
    - remove unneccessary logs
    - remove unused function

--- src/mediafire/mediafire.go
+++ src/mediafire/mediafire.go
@@ -27,10 +27,6 @@ type SessionToken struct {
 	Result    string `xml:"result"`
 }
 
-func (t *SessionToken) SetSecretKey(secret_key string) {
-	t.SecretKey = secret_key
-}
-
 type FoldersContent struct {
 	Action        string        `xml:"action"`
 	FolderContent FolderContent `xml:"folder_content"`
@@ -78,14 +74,54 @@ type File struct {
 	Hash     string   `xml:"hash"`
 	Link     Link     `xml:"links"`
 }
+
 type Link struct {
 	XMLName      xml.Name `xml:"links"`
 	View         string   `xml:"view"`
 	DownloadLink string   `xml:"normal_download"`
 }
 
+func (m *Mediafire) CalculateMediafireSignature(url string) string {
+	secret, _ := strconv.Atoi(m.Token.SecretKey)
+	mod := secret % 256
+	prefix := fmt.Sprintf("%v", mod) + m.Token.Time
+	hash := md5.Sum([]byte(prefix + url))
+	hashResult := hex.EncodeToString(hash[:])
+	//set the new key for next call
+	new_secret := fmt.Sprintf("%v", secret*16807%2147483647)
+	m.Token.SecretKey = new_secret
+	return hashResult
+}
+
+func (m *Mediafire) RetrieveMediafireToken(user common.User) error {
+	mediafireAPIKey := os.Getenv("MEDIAFIRE_API_KEY")
+	applicationID := os.Getenv("MEDIAFIRE_API_ID")
+	hashValue := user.Email + user.Password + applicationID + mediafireAPIKey
+	h := sha1.New()
+	h.Write([]byte(hashValue))
+	signature := hex.EncodeToString(h.Sum(nil))
+	resp, err := http.Get("https://www.mediafire.com/api/1.1/user/get_session_token.php?email=" + user.Email + "&password=" + user.Password + "&application_id=" + applicationID + "&signature=" + signature + "&token_version=2")
+	body, err := ioutil.ReadAll(resp.Body)
+	err = xml.Unmarshal(body, &m.Token)
+	return err
+}
+
+func (m *Mediafire) ReadRootMediafireFolderTree() common.Folder {
+	m_common_root_folder := common.Folder{Name: "", Branch: ""}
+	//Init the parent folder call
+	url_retrieve_parent_folder := "/api/1.1/folder/get_content.php?session_token=" + m.Token.Token + "&content_type=folders"
+	signature_mediafire := m.CalculateMediafireSignature(url_retrieve_parent_folder)
+	mediafire_root_folders_content := RetrieveFoldersFromMediafire(url_retrieve_parent_folder, signature_mediafire)
+	m.ListFoldersWithFilesFromMediafire(url_retrieve_parent_folder, &mediafire_root_folders_content, &m_common_root_folder)
+	//Init the parent file call
+	url_retrieve_root_files := "/api/1.1/folder/get_content.php?session_token=" + m.Token.Token + "&content_type=files"
+	signature_mediafire_files := m.CalculateMediafireSignature(url_retrieve_root_files)
+	mediafire_file_content := RetrieveFilesFromMediafire(url_retrieve_root_files, signature_mediafire_files)
+	m.MediafireFilesContentMapper(mediafire_file_content, &m_common_root_folder)
+	return m_common_root_folder
+}
+
 func RetrieveFoldersFromMediafire(url string, signature string) FoldersContent {
-	log.Println("Folder Request to http://www.mediafire.com" + url + "&signature=" + signature)
 	resp, err := http.Get("http://www.mediafire.com" + url + "&signature=" + signature)
 	common.ErrorHandler("Get folder content from mediafire failed", err)
 	body, err := ioutil.ReadAll(resp.Body)
@@ -98,7 +134,6 @@ func RetrieveFoldersFromMediafire(url string, signature string) FoldersContent {
 }
 
 func RetrieveFilesFromMediafire(url string, signature string) FilesContent {
-	log.Println("File Request to http://www.mediafire.com" + url + "&signature=" + signature)
 	resp, err := http.Get("http://www.mediafire.com" + url + "&signature=" + signature)
 	common.ErrorHandler("Get files content from mediafire failed", err)
 	body, err := ioutil.ReadAll(resp.Body)
@@ -111,7 +146,6 @@ func RetrieveFilesFromMediafire(url string, signature string) FilesContent {
 }
 
 func MediafireFoldersContentMapper(mediafire_folders_content FoldersContent, parent_folder *common.Folder) {
-	log.Println("mediafireFoldersContentMapper List folder size mediafire: " + fmt.Sprintf("%v", len(mediafire_folders_content.FolderContent.Folders.FolderList)))
 	child_folders := []common.Folder{}
 	for _, folder_content := range mediafire_folders_content.FolderContent.Folders.FolderList {
 		child_folder := common.Folder{Key: folder_content.FolderKey, Name: folder_content.Name, Branch: parent_folder.Branch + "/" + folder_content.Name}
@@ -142,7 +176,6 @@ func (m *Mediafire) ListFoldersWithFilesFromMediafire(url string, mediafire_fold
 		if folder.FileCount != 0 {
 			url_retrieve_files := "/api/1.1/folder/get_content.php?folder_key=" + folder.FolderKey + "&session_token=" + m.Token.Token + "&content_type=files"
 			signature_mediafire := m.CalculateMediafireSignature(url_retrieve_files)
-			log.Println("After set mediafire new key:" + m.Token.SecretKey)
 			mediafire_file_content := RetrieveFilesFromMediafire(url_retrieve_files, signature_mediafire)
 			MediafireFilesContentMapper(mediafire_file_content, &child_folder)
 		}
@@ -150,7 +183,6 @@ func (m *Mediafire) ListFoldersWithFilesFromMediafire(url string, mediafire_fold
 		if folder.FolderCount != 0 {
 			url_retrieve_folder := "/api/1.1/folder/get_content.php?folder_key=" + folder.FolderKey + "&session_token=" + m.Token.Token + "&content_type=folders"
 			signature_mediafire := m.CalculateMediafireSignature(url_retrieve_folder)
-			log.Println("After set mediafire new key:" + m.Token.SecretKey)
 			mediafire_child_folder_content := RetrieveFoldersFromMediafire(url_retrieve_folder, signature_mediafire)
 			MediafireFoldersContentMapper(mediafire_child_folder_content, &child_folder)
 			m.ListFoldersWithFilesFromMediafire(url_retrieve_folder, &mediafire_child_folder_content, &child_folder)
@@ -160,44 +192,4 @@ func (m *Mediafire) ListFoldersWithFilesFromMediafire(url string, mediafire_fold
 		}
 	}
 	m_parent_folder.SetFolders(child_folders)
-}
-
-func (m *Mediafire) ReadRootMediafireFolderTree() common.Folder {
-	m_common_root_folder := common.Folder{Name: "root", IsParent: true, Branch: ""}
-	//Init the parent folder call
-	url_retrieve_parent_folder := "/api/1.1/folder/get_content.php?session_token=" + m.Token.Token + "&content_type=folders"
-	signature_mediafire := m.CalculateMediafireSignature(url_retrieve_parent_folder)
-	mediafire_root_folders_content := RetrieveFoldersFromMediafire(url_retrieve_parent_folder, signature_mediafire)
-	m.ListFoldersWithFilesFromMediafire(url_retrieve_parent_folder, &mediafire_root_folders_content, &m_common_root_folder)
-	//Init the parent file call
-	url_retrieve_root_files := "/api/1.1/folder/get_content.php?session_token=" + m.Token.Token + "&content_type=files"
-	signature_mediafire_files := m.CalculateMediafireSignature(url_retrieve_root_files)
-	mediafire_file_content := RetrieveFilesFromMediafire(url_retrieve_root_files, signature_mediafire_files)
-	MediafireFilesContentMapper(mediafire_file_content, &m_common_root_folder)
-	return m_common_root_folder
-}
-
-func (m *Mediafire) CalculateMediafireSignature(url string) string {
-	secret, _ := strconv.Atoi(m.Token.SecretKey)
-	mod := secret % 256
-	prefix := fmt.Sprintf("%v", mod) + m.Token.Time
-	hash := md5.Sum([]byte(prefix + url))
-	hashResult := hex.EncodeToString(hash[:])
-	//set the new key for next call
-	new_secret := fmt.Sprintf("%v", secret*16807%2147483647)
-	m.Token.SetSecretKey(new_secret)
-	return hashResult
-}
-
-func (m *Mediafire) RetrieveMediafireToken(user common.User) error {
-	mediafireAPIKey := os.Getenv("MEDIAFIRE_API_KEY")
-	applicationID := os.Getenv("MEDIAFIRE_API_ID")
-	hashValue := user.Email + user.Password + applicationID + mediafireAPIKey
-	h := sha1.New()
-	h.Write([]byte(hashValue))
-	signature := hex.EncodeToString(h.Sum(nil))
-	resp, err := http.Get("https://www.mediafire.com/api/1.1/user/get_session_token.php?email=" + user.Email + "&password=" + user.Password + "&application_id=" + applicationID + "&signature=" + signature + "&token_version=2")
-	body, err := ioutil.ReadAll(resp.Body)
-	err = xml.Unmarshal(body, &m.Token)
-	return err
 }
